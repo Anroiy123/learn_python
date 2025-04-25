0577f523176692ea3204f52961c8b6e959fe2eb0
Trần Hùng <hunganroiy123@gmail.com>
2025-04-25 21:15:46 +0700

    up file

---
+++ test.py
@@ -0,0 +1,18 @@
+'''
+khởi tạo pygame nhỏ 
+'''
+
+import pygame
+import random
+import math
+import time
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.patches as patches
+
+
+# Khởi tạo pygame
+pygame.init()
